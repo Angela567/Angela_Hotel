0ea573c7dad655ae9d16fb85e942dde862a0caf9
Angela567 <ange.roacaro77@gmail.com>
2025-06-06 19:21:23 -0500

    Primer commit del proyecto ASP.NET MVC Angela_Hotel

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Angela_Hotel.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Angela_Hotel.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
